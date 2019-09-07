ed6dcf2f109bb48cdee18b7916b5d802bbf25ff0
lizdoyle <lizdoyle1121@gmail.com>
2019-09-06 23:06:39 -0400

    fixed clear completed :)

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -4,7 +4,7 @@ const Todo = props => {
     return(
         <div className={`todo${props.todo.clearCompleted ? "clear" : ""}`}
                 onClick={() => {
-                    props.clearCompleted(props.todo.id);
+                    props.clearCompleted(props.todo.completed);
                 }}
                 >
             <p>{props.todo.task}</p>
